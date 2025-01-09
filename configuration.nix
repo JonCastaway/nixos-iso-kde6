b659fc153a98fd479f15c87ff22a786f858b989e
Johnny Castaway <Johnny.Castaway@cybirdtech.com>
2025-01-09 16:28:32 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,27 +1,25 @@
 { pkgs, lib, nixpkgs, ... }:
 
 {
-
-# Enable bcachefs support with the latest kernel packages
+  # Enable bcachefs support with the latest kernel packages
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  
   services.xserver.desktopManager.plasma6 = {
-    kernelPackages = pkgs.linuxPackages_latest + [ pkgs.bcachefs ];
     supportedFilesystems = lib.mkForce [ "bcachefs" "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
   };
 
-# System packages to be installed
+  # System packages to be installed
   environment.systemPackages = with pkgs; [
     neovim
     fastfetch
     man-pages
     man-pages-posix
     keyutils # Required for bcachefs encryption
-
-# Add more packages here as needed
+    # Add more packages here as needed
   ];
 
-# Nix registry configuration for running specific commands
+  # Nix registry configuration for running specific commands
   nix.registry = {
     n.flake = nixpkgs;
   };
-
 }
