3b522d0e1bb82c714e1c7076dffa083a8ac5d587
Johnny Castaway <Johnny.Castaway@cybirdtech.com>
2025-01-09 16:16:23 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,9 @@
-({ pkgs, lib, nixpkgs, ... }: {
+{ pkgs, lib, nixpkgs, ... }:
+
+{
   # Enable bcachefs support
-  boot.kernelPackages = pkgs.linuxPackages_latest.boot.kernelPackages + [ pkgs.bcachefs ];
-  boot.supportedFilesystems = lib.mkForce [ "bcachefs" "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
+  services.xserver.desktopManager.plasma6.kernelPackages = pkgs.linuxPackages_latest.boot.kernelPackages + [ pkgs.bcachefs ];
+  services.xserver.desktopManager.plasma6.supportedFilesystems = lib.mkForce [ "bcachefs" "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
 
   # Choose the system packages you want installed here.
   environment.systemPackages = with pkgs; [
@@ -16,4 +18,4 @@
   nix.registry = {
     n.flake = nixpkgs;
   };
-})
+}
