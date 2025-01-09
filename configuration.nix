1d030ac765e547e0d0c9a5dec7283f6adf47c43a
Johnny Castaway <Johnny.Castaway@cybirdtech.com>
2025-01-09 16:01:17 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,7 @@
 ({ pkgs, lib, nixpkgs, ... }: {
-  # bcachefs support
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.supportedFilesystems = lib.mkForce [ "bcachefs" "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs"];
+  # Enable bcachefs support
+  boot.kernelPackages = pkgs.linuxPackages_latest.boot.kernelPackages + [ pkgs.bcachefs ];
+  boot.supportedFilesystems = lib.mkForce [ "bcachefs" "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
 
   # Choose the system packages you want installed here.
   environment.systemPackages = with pkgs; [
@@ -12,7 +12,7 @@
     keyutils # for bcachefs encryption
   ];
 
-  # supports things like `nix run n#bash`
+  # Supports things like `nix run n#bash`
   nix.registry = {
     n.flake = nixpkgs;
   };
